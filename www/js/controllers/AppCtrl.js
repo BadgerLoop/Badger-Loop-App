b8bd294f6f019854e8b769367f91a1ec2387137d
Kyle <kyle@eatstreet.com>
2016-02-21 22:29:46 -0600

    add ionic deploy

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,7 +1,16 @@
 ﻿app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout) {
     // Form data for the login modal
+
+
     $scope.loginData = {};
 
+
+    var deploy = new Ionic.Deploy();
+    deploy.watch().then(function() {}, function() {},
+    function(hasUpdate) {
+        $scope.doUpdate();
+    });
+    
     var navIcons = document.getElementsByClassName('ion-navicon');
     for (var i = 0; i < navIcons.length; i++) {
         navIcons.addEventListener('click', function () {
@@ -29,4 +38,29 @@
     $scope.$on('$destroy', function () {
         $scope.popover.remove();
     });
+
+  var deploy = new Ionic.Deploy();
+  
+  // Update app code with new release from Ionic Deploy
+  $scope.doUpdate = function() {
+    deploy.update().then(function(res) {
+      console.log('Ionic Deploy: Update Success! ', res);
+    }, function(err) {
+      console.log('Ionic Deploy: Update error! ', err);
+    }, function(prog) {
+      console.log('Ionic Deploy: Progress... ', prog);
+    });
+  };
+
+  // Check Ionic Deploy for new code
+  $scope.checkForUpdates = function() {
+    console.log('Ionic Deploy: Checking for updates');
+    deploy.check().then(function(hasUpdate) {
+      console.log('Ionic Deploy: Update available: ' + hasUpdate);
+      $scope.hasUpdate = hasUpdate;
+    }, function(err) {
+      console.error('Ionic Deploy: Unable to check for updates', err);
+    });
+  }
+
 });

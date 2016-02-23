00f7ab65cdcc8469bf332896c9779c6bbffde0d9
croossin <croossin94@gmail.com>
2016-02-22 18:24:53 -0600

    Added Exis components

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('badger-loop', ['ionic','ionic.service.core', 'ionic-material']);
+var app = angular.module('badger-loop', ['ionic','ionic.service.core', 'ionic-material', 'ngRiffle']);
 
 app.run(function ($ionicPlatform) {
 
@@ -92,4 +92,9 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/components');
+})
+
+// Config for Exis
+.config(function($riffleProvider){
+  $riffleProvider.SetDomain("xs.demo.test");
 });

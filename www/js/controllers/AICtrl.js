043a124746eb43faf7e6730e41248aa3b4730f73
croossin <croossin94@gmail.com>
2016-02-24 14:45:28 -0600

    Fixed chat

--- www/js/controllers/AICtrl.js
+++ www/js/controllers/AICtrl.js
@@ -7,52 +7,35 @@ app
   var alternate,
     isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
 
-  //Formats and sends message to UI
-  // $scope.sendMsg = function(id, message){
-  //   var d = new Date();
-  //   d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
-
-  //   $scope.messages.push({
-  //     userId: id,
-  //     text: message,
-  //     time: d
-  //   });
-  // }  
-
-  // //Called when response from backend is received
-  // $scope.returnMsg = function(s){
-  //   $scope.sendMsg('54321', s);
-  // }
-
-  // //Called when user inputs message in UI
-  // $scope.userMessage = function() {
-    
-  //   $scope.sendMsg('12345', $scope.data.message);
-
-  //   //Send to Exis node the message and get response
-  //   var p = $riffle.call("message", $scope.data.message);
-  //   p.then($scope.returnMsg);
-
-  //   delete $scope.data.message;
-  //   $ionicScrollDelegate.scrollBottom(true);
-  // };
-  $scope.userMessage = function() {
-    alternate = !alternate;
-
+  // Formats and sends message to UI
+  $scope.sendMsg = function(id, message){
     var d = new Date();
-  d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+    d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
 
     $scope.messages.push({
-      userId: alternate ? '12345' : '54321',
-      text: $scope.data.message,
+      userId: id,
+      text: message,
       time: d
     });
+  }  
+
+  //Called when response from backend is received
+  $scope.returnMsg = function(s){
+    $scope.sendMsg('54321', s);
+  }
+
+  //Called when user inputs message in UI
+  $scope.userMessage = function() {
+    
+    $scope.sendMsg('12345', $scope.data.message);
+
+    //Send to Exis node the message and get response
+    var p = $riffle.call("message", $scope.data.message);
+    p.then($scope.returnMsg);
 
     delete $scope.data.message;
     $ionicScrollDelegate.scrollBottom(true);
-
   };
-
 
   $scope.inputUp = function() {
     if (isIOS) $scope.data.keyboardHeight = 216;
@@ -73,7 +56,7 @@ app
 
 
   $scope.data = {};
-  $scope.myId = '12345';
+  $scope.myId = '54321';
   $scope.messages = [];
 
 });

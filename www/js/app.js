ab9d2df90692325ce3af432c05a6bba9115bc378
croossin <croossin94@gmail.com>
2016-03-05 10:17:32 -0600

    Added exis - time to wire up app

--- www/js/app.js
+++ www/js/app.js
@@ -5,136 +5,144 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'nl2br', 'monospaced.elastic'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'nl2br', 'monospaced.elastic', 'ngRiffle'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $riffleProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    //Attach to Exis prod node
+    $riffleProvider.setDomain("xs.demo.badgerloop.blapp.Container.blai");
 
-  // login screen
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'AuthCtrl'
-  })
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // register screen
-  .state('register', {
-    url: '/register',
-    templateUrl: 'templates/register.html',
-    controller: 'AuthCtrl'
-  })
+    // login screen
+        .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'AuthCtrl'
+    })
 
-  // website screen
-  .state('website', {
-    url: '/website',
-    templateUrl: 'templates/website.html',
-    controller: 'AuthCtrl'
-  })
+    // register screen
+    .state('register', {
+        url: '/register',
+        templateUrl: 'templates/register.html',
+        controller: 'AuthCtrl'
+    })
 
-  // vr screen
-  .state('vr', {
-    url: '/vr',
-    templateUrl: 'templates/vr.html',
-    controller: 'AuthCtrl'
-  })
+    // website screen
+    .state('website', {
+        url: '/website',
+        templateUrl: 'templates/website.html',
+        controller: 'AuthCtrl'
+    })
 
-  // Fund screen
-  .state('fundus', {
-    url: '/fundus',
-    templateUrl: 'templates/fundus.html',
-    controller: 'AuthCtrl'
-  })
-  // Home screen
-  .state('home', {
-    url: '/home',
-    templateUrl: 'templates/home.html',
-    controller: 'HomeCtrl'
-  })
+    // vr screen
+    .state('vr', {
+        url: '/vr',
+        templateUrl: 'templates/vr.html',
+        controller: 'AuthCtrl'
+    })
 
-  // Recent posts
-  .state('recent_posts', {
-    url: '/recent-posts',
-    templateUrl: 'templates/recent-posts.html',
-    controller: 'HomeCtrl'
-  })
+    // Fund screen
+    .state('fundus', {
+            url: '/fundus',
+            templateUrl: 'templates/fundus.html',
+            controller: 'AuthCtrl'
+        })
+        // Home screen
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/home.html',
+            controller: 'HomeCtrl'
+        })
 
-  // Wall post
-  .state('wall_posts', {
-    url: '/wall-posts',
-    templateUrl: 'templates/wall-posts.html',
-    controller: 'HomeCtrl'
-  })
+    // Recent posts
+    .state('recent_posts', {
+        url: '/recent-posts',
+        templateUrl: 'templates/recent-posts.html',
+        controller: 'HomeCtrl'
+    })
 
-  // View post detail
-  .state('post', {
-    url: '/post/:postId',
-    templateUrl: 'templates/post.html',
-    controller: 'PostCtrl'
-  })
+    // Wall post
+    .state('wall_posts', {
+        url: '/wall-posts',
+        templateUrl: 'templates/wall-posts.html',
+        controller: 'HomeCtrl'
+    })
 
-  // Chat list
-  .state('chats', {
-    url: '/chats',
-    templateUrl: 'templates/chats.html',
-    controller: 'ChatCtrl'
-  })
+    // View post detail
+    .state('post', {
+        url: '/post/:postId',
+        templateUrl: 'templates/post.html',
+        controller: 'PostCtrl'
+    })
 
-  .state('chat-detail', {
-    url: '/chats/:chatId',
-    templateUrl: 'templates/chat-detail.html',
-    controller: 'ChatDetailCtrl'
-  })
+    // Chat list
+    .state('chats', {
+        url: '/chats',
+        templateUrl: 'templates/chats.html',
+        controller: 'ChatCtrl'
+    })
 
-  // List of notifications
-  .state('notifications', {
-    url: '/notifications',
-    templateUrl: 'templates/notifications.html',
-    controller: 'NotificationsCtrl'
-  })
+    .state('chat-detail', {
+        url: '/chats/:chatId',
+        templateUrl: 'templates/chat-detail.html',
+        controller: 'ChatDetailCtrl'
+    })
 
-  // Team list
-  .state('team', {
-    url: '/team',
-    templateUrl: 'templates/team.html',
-    controller: 'TeamCtrl'
-  })
+    // List of notifications
+    .state('notifications', {
+        url: '/notifications',
+        templateUrl: 'templates/notifications.html',
+        controller: 'NotificationsCtrl'
+    })
 
-  // User profile
-  .state('user', {
-    url: '/user/:userId',
-    templateUrl: 'templates/user.html',
-    controller: 'UserCtrl'
-  })
+    // Team list
+    .state('team', {
+        url: '/team',
+        templateUrl: 'templates/team.html',
+        controller: 'TeamCtrl'
+    })
 
-  // Setting page
-  .state('setting', {
-    url: '/setting',
-    templateUrl: 'templates/setting.html',
-    controller: 'SettingCtrl'
-  })
+    // User profile
+    .state('user', {
+        url: '/user/:userId',
+        templateUrl: 'templates/user.html',
+        controller: 'UserCtrl'
+    })
+
+    // Setting page
+    .state('setting', {
+        url: '/setting',
+        templateUrl: 'templates/setting.html',
+        controller: 'SettingCtrl'
+    })
 
     // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 
+})
+
+.run(function($riffle) {
+    $riffle.setToken("hUzpwpthlJWeHG9Kb9W8yhWodm-sYQvOijNMYqY2mOxoFfZ2BZb8E.QWd1NOxCVFhW249ODPVfZ2tZZk4ke8N.h.wochywzF.PNrYWsPwYDjfsTCfSmXx1JUAW.29uwwh2GirelkMABQ5ynckHtCwTxYoT39K5nkxtjoA-VWHN4_");
+    $riffle.join();
 });

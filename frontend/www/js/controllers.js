1f8a174b15603c0059a360589fd5cd4d667c8309
croossin <croossin94@gmail.com>
2016-03-06 13:36:05 -0600

    name change

--- frontend/www/js/controllers.js
+++ frontend/www/js/controllers.js
@@ -21,7 +21,7 @@ angular.module('starter.controllers', [])
         { image: 'img/BadgerLoopLogo.png', link: 'https://sketchfab.com/models/3767339accc642268061291457c2a21f/embed?autostart=1', name: 'BadgerLoop Logo'}
     ];
 
-    $scope.items = [
+    $scope.timeline = [
       { id: 1, title: 'Title 1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus laoreet lacus quis tempor.', icon: 'icon ion-home', time: '28-08-2015'},
       { id: 2, title: 'Title 2', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices aliquam est sed pharetra.', icon: 'icon ion-ios-heart', time: '20-08-2015'},
       { id: 3, img: 'http://41.media.tumblr.com/c0f54d52d7070a3801b4d926cd4ba71e/tumblr_nphz25UrRh1qd3hr1o1_1280.jpg', title: 'Title 3', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a velit ac eros suscipit lobortis.', icon: 'icon ion-ios-star', time: '19-08-2015'},

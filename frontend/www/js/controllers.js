022fc66867dc43dc876abed8c6dd4428f13f1177
croossin <croossin94@gmail.com>
2016-03-06 12:27:17 -0600

    Working on card functionallity

--- frontend/www/js/controllers.js
+++ frontend/www/js/controllers.js
@@ -3,17 +3,46 @@ angular.module('starter.controllers', [])
 // Authentication controller
 // Put your login, register functions here
 .controller('AuthCtrl', function($scope, $ionicHistory, $ionicSideMenuDelegate) {
-  // hide back butotn in next view
-  $ionicHistory.nextViewOptions({
-    disableBack: true
-  });
+    // hide back butotn in next view
+    $ionicHistory.nextViewOptions({
+        disableBack: true
+    });
 
-  // disabled swipe menu
-  $ionicSideMenuDelegate.canDragContent(false);
+    // disabled swipe menu
+    $ionicSideMenuDelegate.canDragContent(false);
 })
-// News controller
-.controller('HomeCtrl', function($scope, Posts, $state) {
 
+.controller('HomeCtrl', function($scope, TDCardDelegate) {
+    console.log('CARDS CTRL');
+    var cardTypes = [
+        { image: 'img/podoverview.png' },
+        { image: 'img/trophy.jpg' },
+        { image: 'img/BadgerLoopLogo.png' }
+    ];
+
+    $scope.cards = Array.prototype.slice.call(cardTypes, 0);
+
+    $scope.cardDestroyed = function(index) {
+        $scope.cards.splice(index, 1);
+    };
+
+    $scope.addCard = function() {
+        var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+        newCard.id = Math.random();
+        $scope.cards.push(angular.extend({}, newCard));
+    }
+})
+
+
+.controller('CardCtrl', function($scope, TDCardDelegate) {
+    $scope.cardSwipedLeft = function(index) {
+        console.log('LEFT SWIPE');
+        $scope.addCard();
+    };
+    $scope.cardSwipedRight = function(index) {
+        console.log('RIGHT SWIPE');
+        $scope.addCard();
+    };
 })
 
 // Render controller
@@ -23,183 +52,181 @@ angular.module('starter.controllers', [])
 
 // News controller
 .controller('NewsCtrl', function($scope, Posts, $state, $cordovaSocialSharing, $http) {
-  // get list posts froms service
-  Posts.all().then(function(data){
-    $scope.posts = data;
-  });
+    // get list posts froms service
+    Posts.all().then(function(data) {
+        $scope.posts = data;
+    });
 
-  $scope.sharePost = function(post){
-    if(post.isArticle){
-      $cordovaSocialSharing
-      .share("Check out the article BadgerLoop was mentioned in!", "The BadgerLoop team knows what they're doing!", null, post.link) // Share via native share sheet
-      .then(function(result) {
-        // Success!
-      }, function(err) {
-        // An error occured. Show a message to the user
-      });
-    }else{
-      $cordovaSocialSharing
-      .share("Check out the BadgerLoop Team!", "The BadgerLoop team knows what they're doing!", null, 'https://badgerloop.com') // Share via native share sheet
-      .then(function(result) {
-        // Success!
-      }, function(err) {
-        // An error occured. Show a message to the user
-      });
+    $scope.sharePost = function(post) {
+        if (post.isArticle) {
+            $cordovaSocialSharing
+                .share("Check out the article BadgerLoop was mentioned in!", "The BadgerLoop team knows what they're doing!", null, post.link) // Share via native share sheet
+                .then(function(result) {
+                    // Success!
+                }, function(err) {
+                    // An error occured. Show a message to the user
+                });
+        } else {
+            $cordovaSocialSharing
+                .share("Check out the BadgerLoop Team!", "The BadgerLoop team knows what they're doing!", null, 'https://badgerloop.com') // Share via native share sheet
+                .then(function(result) {
+                    // Success!
+                }, function(err) {
+                    // An error occured. Show a message to the user
+                });
+        }
     }
-  }
 
-  // view post
-  $scope.viewPost = function(post) {
-    //Open in different viewer
-    if(post.isArticle){
-      $state.go('articles', {link: post.link, name: post.name});
-    }else{
-      $state.go('post', {postId: post.id});
-    } 
-  }
+    // view post
+    $scope.viewPost = function(post) {
+        //Open in different viewer
+        if (post.isArticle) {
+            $state.go('articles', { link: post.link, name: post.name });
+        } else {
+            $state.go('post', { postId: post.id });
+        }
+    }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats, $ionicScrollDelegate, $ionicActionSheet, $timeout) {
-  $scope.chatadd = Chats;
-  $scope.chat = Chats.get(0);
+    $scope.chatadd = Chats;
+    $scope.chat = Chats.get(0);
 
-  $scope.sendMessage = function() {
-    var d = new Date();
-    var message = {
-      type: 'sent',
-      time: d.toLocaleTimeString(),
-      text: $scope.input.message
+    $scope.sendMessage = function() {
+        var d = new Date();
+        var message = {
+            type: 'sent',
+            time: d.toLocaleTimeString(),
+            text: $scope.input.message
+        };
+
+        $scope.input.message = '';
+
+        // push to massages list
+        Chats.addMsg(message);
     };
 
-    $scope.input.message = '';
+    // hover menu
+    $scope.onMessageHold = function(e, itemIndex, message) {
+        // show hover menu
+        $ionicActionSheet.show({
+            buttons: [{
+                text: 'Copy Text'
+            }, {
+                text: 'Delete Message'
+            }],
+            buttonClicked: function(index) {
+                switch (index) {
+                    case 0: // Copy Text
+                        //cordova.plugins.clipboard.copy(message.text);
 
-    // push to massages list
-    Chats.addMsg(message);
-  };
+                        break;
+                    case 1: // Delete
+                        // no server side secrets here :~)
+                        $scope.chat.messages.splice(itemIndex, 1);
+                        break;
+                }
 
-  // hover menu
-  $scope.onMessageHold = function(e, itemIndex, message) {
-    // show hover menu
-    $ionicActionSheet.show({
-      buttons: [
-        {
-          text: 'Copy Text'
-        }, {
-          text: 'Delete Message'
-        }
-      ],
-      buttonClicked: function(index) {
-        switch (index) {
-          case 0: // Copy Text
-            //cordova.plugins.clipboard.copy(message.text);
-
-            break;
-          case 1: // Delete
-            // no server side secrets here :~)
-            $scope.chat.messages.splice(itemIndex, 1);
-            break;
-        }
-
-        return true;
-      }
-    });
-  };
+                return true;
+            }
+        });
+    };
 
 })
 
 .controller('PostCtrl', function($scope, Posts, $state, $stateParams) {
 
-  console.log($stateParams.postId);
+    console.log($stateParams.postId);
 
-  // get list posts froms service
-  $scope.post = Posts.get($stateParams.postId);
+    // get list posts froms service
+    $scope.post = Posts.get($stateParams.postId);
 })
 
 .controller('ArticlesCtrl', function($scope, Posts, $state, $stateParams, $sce) {
-  // set up variables
-  $scope.link = $sce.trustAsResourceUrl($stateParams.link);
-  $scope.name = $stateParams.name;
-  console.log($scope.link, $scope.name);
+    // set up variables
+    $scope.link = $sce.trustAsResourceUrl($stateParams.link);
+    $scope.name = $stateParams.name;
+    console.log($scope.link, $scope.name);
 })
 
 // Notifications controller
 .controller('NotificationsCtrl', function($scope, Notifications) {
-  // get list posts from service
-  $scope.notifications = Notifications.all();
+    // get list posts from service
+    $scope.notifications = Notifications.all();
 })
 
 // ContactsCtrl controller
 .controller('TeamCtrl', function($scope, Contacts, $state) {
-  // get list posts froms service
-  $scope.contacts = Contacts.all();
+    // get list posts froms service
+    $scope.contacts = Contacts.all();
 
-  // view contact function
-  $scope.viewContact = function(contactId) {
-    $state.go('user', {userId: contactId});
-  }
+    // view contact function
+    $scope.viewContact = function(contactId) {
+        $state.go('user', { userId: contactId });
+    }
 })
 
 // Funding controller
-.controller('FundCtrl', function($scope, StripeCharge){
-  $scope.ProductMeta = {
-    title: "BadgerLoop Fundraising",
-    description: "Thank you!",
-    priceUSD: 1,
-  };
+.controller('FundCtrl', function($scope, StripeCharge) {
+    $scope.ProductMeta = {
+        title: "BadgerLoop Fundraising",
+        description: "Thank you!",
+        priceUSD: 1,
+    };
 
-  $scope.status = {
-    loading: false,
-    message: "",
-  };
+    $scope.status = {
+        loading: false,
+        message: "",
+    };
 
-  $scope.charge = function(amount) {
-    if(typeof amount === "string"){
-      $scope.ProductMeta.priceUSD = parseInt(amount);
-    }else{
-      $scope.ProductMeta.priceUSD = amount;  
-    }
-    console.log($scope.ProductMeta.priceUSD);
-    $scope.status['loading'] = true;
-    $scope.status['message'] = "Retrieving your Stripe Token...";
-
-    // first get the Stripe token
-    StripeCharge.getStripeToken($scope.ProductMeta).then(
-      function(stripeToken){
-        // -->
-        proceedCharge(stripeToken);
-      },
-      function(error){
-        console.log(error)
-
-        $scope.status['loading'] = false;
-        if(error != "ERROR_CANCEL") {
-          $scope.status['message'] = "Oops... something went wrong";
+    $scope.charge = function(amount) {
+        if (typeof amount === "string") {
+            $scope.ProductMeta.priceUSD = parseInt(amount);
         } else {
-          $scope.status['message'] = "";
+            $scope.ProductMeta.priceUSD = amount;
         }
-      }
-    ); // ./ getStripeToken
+        console.log($scope.ProductMeta.priceUSD);
+        $scope.status['loading'] = true;
+        $scope.status['message'] = "Retrieving your Stripe Token...";
 
-    function proceedCharge(stripeToken) {
+        // first get the Stripe token
+        StripeCharge.getStripeToken($scope.ProductMeta).then(
+            function(stripeToken) {
+                // -->
+                proceedCharge(stripeToken);
+            },
+            function(error) {
+                console.log(error)
 
-      $scope.status['message'] = "Processing your payment...";
+                $scope.status['loading'] = false;
+                if (error != "ERROR_CANCEL") {
+                    $scope.status['message'] = "Oops... something went wrong";
+                } else {
+                    $scope.status['message'] = "";
+                }
+            }
+        ); // ./ getStripeToken
 
-      // then chare the user through your custom node.js server (server-side)
-      StripeCharge.chargeUser(stripeToken, $scope.ProductMeta).then(
-        function(StripeInvoiceData){
-          $scope.status['loading'] = false;
-          $scope.status['message'] = "Success! Check your Stripe Account";
-          console.log(StripeInvoiceData)
-        },
-        function(error){
-          console.log(error);
+        function proceedCharge(stripeToken) {
 
-          $scope.status['loading'] = false;
-          $scope.status['message'] = "Oops... something went wrong";
-        }
-      );
+            $scope.status['message'] = "Processing your payment...";
 
-    }; // ./ proceedCharge
+            // then chare the user through your custom node.js server (server-side)
+            StripeCharge.chargeUser(stripeToken, $scope.ProductMeta).then(
+                function(StripeInvoiceData) {
+                    $scope.status['loading'] = false;
+                    $scope.status['message'] = "Success! Check your Stripe Account";
+                    console.log(StripeInvoiceData)
+                },
+                function(error) {
+                    console.log(error);
 
-  };
+                    $scope.status['loading'] = false;
+                    $scope.status['message'] = "Oops... something went wrong";
+                }
+            );
+
+        }; // ./ proceedCharge
+
+    };
 })
